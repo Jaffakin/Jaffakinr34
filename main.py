4c5bbd25f4d1adaa223e2f1449c17b46535d1a80
Morgan <27170241+MorganIsCute@users.noreply.github.com>
2018-06-20 15:06:33 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from images.nsfw.hentai import hentai
 from images.sfw.google import images
+from images.nsfw.lewd import lewd
 from utils.core import core
 
 from discord.ext import commands
@@ -13,25 +13,38 @@ class main():
 		self.bot = bot
 		self.core = core(bot)
 
+		self.lewd = None
 		self.images = None
-		self.hentai = None
 
 		self.build_modules()
 
 	def build_modules(self):
 
+		self.lewd = lewd(self.bot, self.core)
 		self.images = images(self.bot, self.core)
-		self.hentai = hentai(self.bot, self.core)
+
+	async def process_author(self, author:int):
+
+		if author == self.bot.user.id:
+
+			return False
+
+		return True
 
 	async def process_message(self, message):
 
 		if message.content.startswith("!"):
 
-			await self.process_command(message)
+			if await self.process_author(message.author.id):
+
+				await self.process_command(message)
 
 	async def process_reaction(self, reaction, user):
 
-		await self.images.process_reaction(reaction, user)
+		if await self.process_author(user.id):
+
+			await self.lewd.react(reaction, user)
+			await self.images.react(reaction, user)
 
 	async def process_command(self, message):
 
@@ -51,5 +64,5 @@ class main():
 	async def exec_command(self, message, alias):
 
 		await self.core.invoke(message, alias)
+		await self.lewd.invoke(message, alias)
 		await self.images.invoke(message, alias)
-		await self.hentai.invoke(message, alias)
